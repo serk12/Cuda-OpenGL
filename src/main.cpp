93317f9925b9f5a2562e485ac769824ef59b4f7b
serk12 <marc.prat.maso@est.fib.upc.edu>
2019-05-17 10:15:06 +0200

    final basic test: -texture with a shader like a flare (cuda) -rotating cube (OpenGL)

--- src/main.cpp
+++ src/main.cpp
@@ -25,6 +25,8 @@ GLuint pbo = 0;     // OpenGL pixel buffer object
 GLuint tex = 0;     // OpenGL texture object
 struct cudaGraphicsResource *cuda_pbo_resource;
 
+GLfloat xRotated, yRotated, zRotated;
+
 void render() {
         uchar4 *d_out = 0;
         cudaGraphicsMapResources(1, &cuda_pbo_resource, 0);
@@ -39,31 +41,32 @@ void drawTexture() {
                      GL_UNSIGNED_BYTE, NULL);
         glEnable(GL_TEXTURE_2D);
         glBegin(GL_QUADS);
-        glTexCoord2f(0.0f, 0.0f); glVertex2f(-W/100, -H/100);
-        glTexCoord2f(0.0f, 1.0f); glVertex2f(-W/100,  H/100);
-        glTexCoord2f(1.0f, 1.0f); glVertex2f(W/100,  H/100);
-        glTexCoord2f(1.0f, 0.0f); glVertex2f(W/100, -H/100);
+        glColor3f(1.0f,1.0f,1.0f);
+        glTexCoord2f(0.0f, 0.0f); glVertex3f(-W/100, -H/100,-3);
+        glTexCoord2f(0.0f, 1.0f); glVertex3f(-W/100,  H/100,-3);
+        glTexCoord2f(1.0f, 1.0f); glVertex3f(W/100,  H/100, -3);
+        glTexCoord2f(1.0f, 0.0f); glVertex3f(W/100, -H/100, -3);
         glEnd();
         glDisable(GL_TEXTURE_2D);
 }
 
 void drawCube() {
-        // glRotatef(xRotated,1.0,0.0,0.0);
-        // glRotatef(yRotated,0.0,1.0,0.0);
-        // glRotatef(zRotated,0.0,0.0,1.0);
+        glRotatef(xRotated,1.0,0.0,0.0);
+        glRotatef(yRotated,0.0,1.0,0.0);
+        glRotatef(zRotated,0.0,0.0,1.0);
 
         glBegin(GL_QUADS);  // Draw The Cube Using quads
-        glColor3f(0.0f,1.0f,0.0f);  // Color Blue
+        glColor3f(1.0f,0.0f,0.0f);  // Color Blue
         glVertex3f( 1.0f, 1.0f,-1.0f);  // Top Right Of The Quad (Top)
         glVertex3f(-1.0f, 1.0f,-1.0f);  // Top Left Of The Quad (Top)
         glVertex3f(-1.0f, 1.0f, 1.0f);  // Bottom Left Of The Quad (Top)
         glVertex3f( 1.0f, 1.0f, 1.0f);  // Bottom Right Of The Quad (Top)
-        glColor3f(1.0f,0.5f,0.0f);  // Color Orange
+        glColor3f(0.0f,1.0f,0.0f);  // Color Orange
         glVertex3f( 1.0f,-1.0f, 1.0f);  // Top Right Of The Quad (Bottom)
         glVertex3f(-1.0f,-1.0f, 1.0f);  // Top Left Of The Quad (Bottom)
         glVertex3f(-1.0f,-1.0f,-1.0f);  // Bottom Left Of The Quad (Bottom)
         glVertex3f( 1.0f,-1.0f,-1.0f);  // Bottom Right Of The Quad (Bottom)
-        glColor3f(1.0f,0.0f,0.0f);  // Color Red
+        glColor3f(0.0f,0.0f,1.0f);  // Color Red
         glVertex3f( 1.0f, 1.0f, 1.0f);  // Top Right Of The Quad (Front)
         glVertex3f(-1.0f, 1.0f, 1.0f);  // Top Left Of The Quad (Front)
         glVertex3f(-1.0f,-1.0f, 1.0f);  // Bottom Left Of The Quad (Front)
@@ -73,7 +76,7 @@ void drawCube() {
         glVertex3f(-1.0f,-1.0f,-1.0f);  // Top Left Of The Quad (Back)
         glVertex3f(-1.0f, 1.0f,-1.0f);  // Bottom Left Of The Quad (Back)
         glVertex3f( 1.0f, 1.0f,-1.0f);  // Bottom Right Of The Quad (Back)
-        glColor3f(0.0f,0.0f,1.0f);  // Color Blue
+        glColor3f(0.0f,1.0f,1.0f);  // Color Blue
         glVertex3f(-1.0f, 1.0f, 1.0f);  // Top Right Of The Quad (Left)
         glVertex3f(-1.0f, 1.0f,-1.0f);  // Top Left Of The Quad (Left)
         glVertex3f(-1.0f,-1.0f,-1.0f);  // Bottom Left Of The Quad (Left)
@@ -88,7 +91,10 @@ void drawCube() {
 
 void draw() {
         glMatrixMode(GL_MODELVIEW);
-        glClear(GL_COLOR_BUFFER_BIT);
+        // glEnable(GL_CULL_FACE);
+        glEnable(GL_DEPTH_TEST);
+        glDepthMask(GL_TRUE);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glLoadIdentity();
         glTranslatef(0.0,0.0,-10.5);
         drawTexture();
@@ -142,6 +148,12 @@ void reshape(int x, int y) {
         glViewport(0,0,x,y); //Use the whole window for rendering
 }
 
+void animation(void) {
+        yRotated += 0.4;
+        xRotated += 0.4;
+        display();
+}
+
 int main(int argc, char** argv) {
         printInstructions();
         initGLUT(&argc, argv);
@@ -153,6 +165,7 @@ int main(int argc, char** argv) {
         initPixelBuffer();
 
         glutReshapeFunc(reshape);
+        glutIdleFunc(animation);
 
         glutMainLoop();
         atexit(exitfunc);
